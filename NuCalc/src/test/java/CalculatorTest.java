7b47f5708afdec2277fafcbac3aed372133d816c
sukrithanda <sukrit.handa@utoronto.ca>
2016-10-20 20:57:18 -0400

    Adding a more extensive set of unit tests. testing all different markup categories

--- NuCalc/src/test/java/CalculatorTest.java
+++ NuCalc/src/test/java/CalculatorTest.java
@@ -70,6 +70,54 @@ public class CalculatorTest {
 		assertEquals(13707.63, testcalc3.CalculateMarkup(),0);
 	}
 	
+	@Test
+	public void testfoodonly(){
+		ArrayList<Markups> m = new ArrayList<Markups>();
+		m.add(Markups.FOOD);
+		m.add(Markups.ELSE);
+		
+		Calculator food1 = new Calculator(1299.99,3,m);
+		assertEquals(1591.58, food1.CalculateMarkup(),0);
+		
+		m.remove(1);
+		
+		Calculator food2 = new Calculator(1299.99,3,m);
+		assertEquals(1591.58, food2.CalculateMarkup(),0);
+
+	}
+	
+	@Test
+	public void testpharmaonly(){
+		ArrayList<Markups> m = new ArrayList<Markups>();
+		m.add(Markups.PHARMA);
+		m.add(Markups.ELSE);
+		
+		Calculator food1 = new Calculator(5432.00,1,m);
+		assertEquals(6199.81, food1.CalculateMarkup(),0);
+		
+		m.remove(1);
+		
+		Calculator food2 = new Calculator(5432.00,1,m);
+		assertEquals(6199.81, food2.CalculateMarkup(),0);
+
+	}
+	
+	@Test
+	public void testelectroniconly(){
+		ArrayList<Markups> m = new ArrayList<Markups>();
+		m.add(Markups.ELECTRONIC);
+		m.add(Markups.ELSE);
+		
+		Calculator elec1 = new Calculator(399.99,11,m);
+		assertEquals(483.83, elec1.CalculateMarkup(),0);
+		
+		m.remove(1);
+		
+		Calculator elec2 = new Calculator(399.99,11,m);
+		assertEquals(483.83, elec2.CalculateMarkup(),0);
+
+	}
+	
 	
 
 
